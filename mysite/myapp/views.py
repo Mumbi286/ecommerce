5966302c505e900a27e4ee0f77a0b61680bcdd66
Christine Mumbi <christinemumbi286@gmail.com>
2026-07-06 11:45:27 +0300

    Added product detail page with add-to-cart button
    - Added get_absolute_url to Product and link product cards on index
    - Built detail template with buy box and AJAX add-to-cart
    - Included jQuery in base template for the cart request
    - Documented views with docstrings

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+
 from .models import Product
 
-# Create your views here.
+
 def index(request):
+    """Render the home page with a list of all products."""
     products = Product.objects.all()
-    return render(request,'myapp/index.html',{'products':products})
+    return render(request, 'myapp/index.html', {'products': products})
+
 
 def detail(request, slug):
+    """Render the detail page for the product matching the URL slug."""
     product = Product.objects.get(slug=slug)
-    return render(request,'myapp/detail.html',{'product':product})
-
-
+    return render(request, 'myapp/detail.html', {'product': product})
