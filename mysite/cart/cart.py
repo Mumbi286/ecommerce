71325c7ed6a52723ea2b8d10767ccfe292ac463f
Christine Mumbi <christinemumbi286@gmail.com>
2026-07-06 22:02:45 +0300

    Add cart overview page with item update and removal
    - Iterated session cart with product details, line totals, and grand total
    - Added cart-overview, update, and delete endpoints with AJAX handlers
    - Registered django-mathfilters for price math in templates

--- mysite/cart/cart.py
+++ mysite/cart/cart.py
@@ -1,3 +1,6 @@
+from myapp.models import Product
+from decimal import Decimal
+
 class Cart():
     def __init__(self,request):
         # creating the session and extracting the session
@@ -10,6 +13,34 @@ class Cart():
             # else load the existing cart
         self.cart = cart
 
+    def __len__(self):
+        return sum (int(item['qty']) for item in self.cart.values())
+    
+    # calculating the total 
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * Decimal(item['qty']) for item in self.cart.values())
+    
+    # converting the cart into iterable
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product']=product
+
+        for item in cart.values():
+            item['price']= Decimal(item['price'])
+            item['qty']= Decimal(item['qty'])
+            item['total']=item['price']*item['qty']
+            yield item
+
+        
+    
+            
+
+
+
 # allows us to add items to add in the cart
     def add(self,product,product_qty):
         product_id = product.id
@@ -21,4 +52,19 @@ class Cart():
             self.cart[product_id]={'price':str(product.price),'qty':product_qty} # self.cart[3] = {'price}:str(80),'qty':1}        
         # modifying our session
         self.session.modified = True 
+
+    # delete method 
+    def delete(self,product_id):
+        product_id = str(product_id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.session.modified=True # makes sure the cart stored into the session is updated
+
+    def update(self,product,qty):
+        product_id = str(product)
+        product_quantity = qty
+        if product_id in self.cart:
+            # accessing the cart
+            self.cart[product_id]['qty']=product_quantity
+        self.session.modified=True
 
--- mysite/cart/views.py
+++ mysite/cart/views.py
@@ -19,5 +19,31 @@ def cart_add(request):
         # product = Product.objects.get(id=product_id)
         product = get_object_or_404(Product,id=product_id)
         cart.add(product=product,product_qty=product_quantity)
+        cart_quantity = cart.__len__()
 
-    return JsonResponse({'Message':'Add to Cart button clicked'})
+    return JsonResponse({'qty':cart_quantity})
+
+def cart_overview(request):
+    cart = Cart(request)
+    return render(request,'cart/cart-overview.html',{'cart':cart})
+
+# Delete function 
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get("action")=='post':
+        product_id = request.POST.get('product_id')
+        cart.delete(product_id=product_id)
+        cart_quantity = cart.__len__()
+        cart_total = cart.get_total_price()
+        return JsonResponse({'qty':cart_quantity,'total':cart_total})
+    
+def cart_update(request):
+    cart = Cart(request)
+    if request.method=="POST":
+        product_id = request.POST.get('product_id')
+        product_quantity = request.POST.get('product_quantity')
+        cart.update(product=product_id,qty=product_quantity)
+        return JsonResponse({"Message":"Product updated"})
+
+    
+
