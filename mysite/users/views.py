5c80a84923c6464bbe49ba4c6a926cf65dc7cb1a
Christine Mumbi <christinemumbi286@gmail.com>
2026-07-08 06:54:15 +0300

    Creating an email verification token

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -8,7 +8,30 @@ def register(request):
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.is_active = False
+            user.save()
+
+
+            # Email verification logic
+            subject = 'Verify your email to activate your accoount'
+            message = ''
+
+
+
             return redirect('index')
         
     return render(request,'users/register.html',{'form':form})
+
+def email_verification(request):
+    pass
+
+
+def email_verification_sent(rquest):
+    pass
+
+def email_verification_success(request):
+    pass
+
+def email_verification_failed(request):
+    pass
